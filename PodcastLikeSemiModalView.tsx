10bc6dc4aca539ff30c6efb9789d40c89594f4c0
Yuta Mori <moriyu54321@gmail.com>
2019-10-13 21:01:52 +0900

    update

--- PodcastLikeSemiModalView.tsx
+++ PodcastLikeSemiModalView.tsx
@@ -4,31 +4,38 @@ import {
   Text,
   View,
   TouchableOpacity,
-  LayoutAnimation,
   Animated,
-  Dimensions,
-  Image,
-  SafeAreaView
+  Dimensions
 } from "react-native";
+import { State, PanGestureHandler } from "react-native-gesture-handler";
 
 const WINDOW_WIDTH = Dimensions.get("window").width;
 const WINDOW_HEIGHT = Dimensions.get("window").height;
 const MINI_HEIGHT = 64;
-const SNAP_POINTS = [
-  57,
-  // 400
-  WINDOW_HEIGHT - MINI_HEIGHT
-];
+const SNAP_POINTS = [57, WINDOW_HEIGHT - MINI_HEIGHT];
 
-type ModalState =
-  | "full"
-  //   | "half"
-  | "mini";
+type ModalState = "full" | "mini";
 
-const PodcastLikeSemiModalView = () => {
+const PodcastLikeSemiModalView: React.FC = () => {
   const [modalState, setModalState] = useState<ModalState>("full");
 
-  const translateY = useMemo(() => new Animated.Value(SNAP_POINTS[0]), []);
+  const dragY = useMemo(() => new Animated.Value(0), []);
+  const lastStopSnapPoint = useMemo(
+    () => new Animated.Value(SNAP_POINTS[0]),
+    []
+  );
+  const translateY = useMemo(
+    () =>
+      Animated.add(
+        lastStopSnapPoint,
+        Animated.multiply(dragY, 0.36)
+      ).interpolate({
+        inputRange: [SNAP_POINTS[0], SNAP_POINTS[1]],
+        outputRange: [SNAP_POINTS[0], SNAP_POINTS[1]],
+        extrapolate: "clamp"
+      }),
+    []
+  );
   const borderRadius = useMemo(
     () =>
       translateY.interpolate({
@@ -95,27 +102,32 @@ const PodcastLikeSemiModalView = () => {
   );
 
   const switchModalState = (state: ModalState) => {
-    if (state === modalState) return;
-
     setModalState(state);
 
     if (state === "full") {
-      Animated.spring(translateY, {
-        toValue: SNAP_POINTS[0],
-        bounciness: 0,
-        useNativeDriver: false
-      }).start();
+      Animated.parallel([
+        Animated.spring(dragY, { toValue: 0, bounciness: 0 }),
+        Animated.spring(lastStopSnapPoint, {
+          toValue: SNAP_POINTS[0],
+          bounciness: 0
+        })
+      ]).start();
     }
     if (state === "mini") {
-      Animated.spring(translateY, {
-        toValue: SNAP_POINTS[1],
-        bounciness: 5,
-        useNativeDriver: false
-      }).start();
+      Animated.parallel([
+        Animated.spring(dragY, { toValue: 0, bounciness: 0 }),
+        Animated.spring(lastStopSnapPoint, {
+          toValue: SNAP_POINTS[1],
+          bounciness: 0
+        })
+      ]).start();
     }
-    // if (state === "mini") {
-    //   Animated.spring(top, { toValue: SNAP_POINTS[2], bounciness: 0 }).start();
-    // }
+  };
+
+  const onHandlerStateChange = ({ nativeEvent }) => {
+    if (nativeEvent.oldState === State.ACTIVE) {
+      switchModalState(nativeEvent.translationY > 214 ? "mini" : "full");
+    }
   };
 
   return (
@@ -145,61 +157,76 @@ const PodcastLikeSemiModalView = () => {
         <Text style={styles.state}>{modalState}</Text>
       </View>
 
-      <Animated.View
-        style={[
-          styles.modalView,
-          {
-            transform: [{ translateY }],
-            borderTopLeftRadius: borderRadius,
-            borderTopRightRadius: borderRadius
-          }
-        ]}
+      <PanGestureHandler
+        onGestureEvent={Animated.event(
+          [{ nativeEvent: { translationY: dragY } }],
+          { useNativeDriver: false }
+        )}
+        onHandlerStateChange={Animated.event([], {
+          listener: onHandlerStateChange
+        })}
       >
-        <TouchableOpacity
-          activeOpacity={1}
-          onPress={() => switchModalState("full")}
+        <Animated.View
+          style={[
+            styles.modalView,
+            {
+              transform: [{ translateY }],
+              borderTopLeftRadius: borderRadius,
+              borderTopRightRadius: borderRadius
+            }
+          ]}
         >
-          <Animated.View
-            style={[
-              styles.full_contentWrapper,
-              { opacity: fullOpacity, paddingTop: fullContentWrapperPaddingTop }
-            ]}
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => switchModalState("full")}
           >
-            <View style={styles.pseudoSeekbar} />
-            <Text style={styles.full_title} numberOfLines={1}>
-              #005 The Gratest Episode in the World
-            </Text>
-            <TouchableOpacity
-              style={[styles.button, { marginTop: 16 }]}
-              onPress={() => switchModalState("mini")}
+            <Animated.View
+              style={[
+                styles.full_contentWrapper,
+                {
+                  opacity: fullOpacity,
+                  paddingTop: fullContentWrapperPaddingTop
+                }
+              ]}
             >
-              <Text>Mini</Text>
-            </TouchableOpacity>
-          </Animated.View>
+              <View style={styles.pseudoSeekbar} />
+              <Text style={styles.full_title} numberOfLines={1}>
+                #005 The Gratest Episode in the World
+              </Text>
+              <TouchableOpacity
+                style={[styles.button, { marginTop: 16 }]}
+                onPress={() => switchModalState("mini")}
+              >
+                <Text>Mini</Text>
+              </TouchableOpacity>
+            </Animated.View>
 
-          <Animated.View style={[styles.mini, { opacity: miniOpacity }]}>
-            <Text style={styles.mini_title} numberOfLines={1}>
-              #005 The Gratest Episode in the World
-            </Text>
-          </Animated.View>
-          <View style={styles.gripBarWrapper}>
-            <Animated.View style={[styles.gripBar, { opacity: fullOpacity }]} />
-          </View>
-          <Animated.Image
-            style={[
-              styles.artwork,
-              {
-                height: artworkSize,
-                width: artworkSize,
-                borderRadius: artworkBorderRadius,
-                top: artworkTop,
-                left: artworkLeft
-              }
-            ]}
-            source={require("./assets/artwork.jpg")}
-          />
-        </TouchableOpacity>
-      </Animated.View>
+            <Animated.View style={[styles.mini, { opacity: miniOpacity }]}>
+              <Text style={styles.mini_title} numberOfLines={1}>
+                #005 The Gratest Episode in the World
+              </Text>
+            </Animated.View>
+            <View style={styles.gripBarWrapper}>
+              <Animated.View
+                style={[styles.gripBar, { opacity: fullOpacity }]}
+              />
+            </View>
+            <Animated.Image
+              style={[
+                styles.artwork,
+                {
+                  height: artworkSize,
+                  width: artworkSize,
+                  borderRadius: artworkBorderRadius,
+                  top: artworkTop,
+                  left: artworkLeft
+                }
+              ]}
+              source={require("./assets/artwork.jpg")}
+            />
+          </TouchableOpacity>
+        </Animated.View>
+      </PanGestureHandler>
     </>
   );
 };
@@ -230,11 +257,7 @@ const styles = StyleSheet.create({
 
   // main
   modalView: {
-    position: "absolute",
-    right: 0,
-    left: 0,
-    bottom: 0,
-    top: 0,
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "#fff"
   },
   mini: {
